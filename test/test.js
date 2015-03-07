6bde5c4b27479dd215fa78511dc3ad994acf32c7
Abdelrahman Barakat <abdelrahman.barakat@gmail.com>
2015-03-07 22:44:35 +0200

    find by exact words

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var imdb = require('../imdb');
+var imdb = require('../imdb.js');
 var chai = require('chai');
 var chaiAsPromised = require('chai-as-promised');
 var expect = chai.expect;
@@ -8,9 +8,16 @@ var expect = chai.expect;
 chai.use(chaiAsPromised);
 
 describe('imdb', function() {
-  describe('#title()', function() {
-    it('should scrap title data by id', function() {
+  describe('title()', function() {
+    it('should scrap The Godfather data by id', function() {
       return expect(imdb.title('tt0068646')).to.eventually.deep.equal(require('./TheGodfather'));
+    });
+
+    describe('find()', function() {
+      it('should find results for The Imitation Game', function() {
+        var epected = require('./TheImitationGameResults');
+        return expect(imdb.title.find('The Imitation Game')).to.eventually.deep.equal(epected);
+      });
     });
   });
 });
